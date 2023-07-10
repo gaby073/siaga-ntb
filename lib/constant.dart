7f44fdd62943d44dea87b01efc3c0c6c1bfabcc8
mpratama17 <yogmhmmd17@gmail.com>
2023-07-10 07:35:44 +0700

    no change

--- lib/constant.dart
+++ lib/constant.dart
@@ -3,5 +3,8 @@ import 'dart:ui';
 import 'package:flutter/material.dart';
 
 class ColorConstants {
-  static Color PrimaryCo = Color(0xFF3AA8EB);
+  static Color PrimaryCo = Color(0xFF3BA6D4);
+  static Color SecondaryCo = Color(0xFF48BEE6);
+  static Color WhiteCo = Color(0xFFFCFFFF);
+  static Color BgCo = Color(0xFFF8F9FE);
 }
--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,8 @@
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
 import 'package:google_fonts/google_fonts.dart';
+import 'package:siaga_ntb/constant.dart';
+import 'package:siaga_ntb/pages/edukasi_bencana.dart';
 import 'package:siaga_ntb/pages/home.dart';
 import 'package:siaga_ntb/pages/nomor_penting.dart';
 import 'package:siaga_ntb/routes/page_route.dart';
@@ -17,11 +19,12 @@ class MyApp extends StatelessWidget {
     return GetMaterialApp(
       debugShowCheckedModeBanner: false,
       theme: ThemeData(
+        scaffoldBackgroundColor: ColorConstants.BgCo,
         textTheme: GoogleFonts.poppinsTextTheme(
           Theme.of(context).textTheme,
         ),
       ),
-      home: const Home(),
+      home: NomorPenting(),
       initialRoute: '/',
       getPages: PageRoutes.pages,
     );
