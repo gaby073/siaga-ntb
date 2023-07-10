8cbe6522ddbb69bfe3de536b7f61b81095ddfd00
mpratama17 <yogmhmmd17@gmail.com>
2023-07-10 07:34:03 +0700

    new models for contact

---
+++ lib/models/list_nomor.dart
@@ -0,0 +1,34 @@
+var TitleNomor = [
+  'Markas PMI Provinsi NTB',
+  'Markas PMI Kabupaten Lombok Barat',
+  'Markas PMI Kabupaten Lombok Tengah',
+  'Markas PMI Kabupaten Lombok Timur',
+  'Markas PMI Kabupaten Lombok Utara',
+  'Markas PMI Kabupaten Sumbawa',
+  'Markas PMI Kabupaten Dompu',
+  'Markas PMI Kabupaten Bima',
+  'Markas PMI Kabupaten Sumbawa Barat',
+  'Markas PMI Kota Mataram',
+  'Markas PMI Kota Bima',
+  'Markas PMI Kota Taliwang',
+  'Markas PMI Kota Selong',
+  'Markas PMI Kota Praya',
+  'Markas PMI Kota Gerung'
+];
+var SubTitleNomor = [
+  'Jl. Pejanggik No. 1, Mataram, NTB',
+  'Jl. Pejanggik No. 1, Mataram, NTB',
+  'Jl. Pejanggik No. 1, Mataram, NTB',
+  'Jl. Pejanggik No. 1, Mataram, NTB',
+  'Jl. Pejanggik No. 1, Mataram, NTB',
+  'Jl. Pejanggik No. 1, Mataram, NTB',
+  'Jl. Pejanggik No. 1, Mataram, NTB',
+  'Jl. Pejanggik No. 1, Mataram, NTB',
+  'Jl. Pejanggik No. 1, Mataram, NTB',
+  'Jl. Pejanggik No. 1, Mataram, NTB',
+  'Jl. Sultan Muhammad Salahuddin No. 1, Bima, NTB',
+  'Jl. Sultan Muhammad Salahuddin No. 1, Bima, NTB',
+  'Jl. Sultan Muhammad Salahuddin No. 1, Bima, NTB',
+  'Jl. Sultan Muhammad Salahuddin No. 1, Bima, NTB',
+  'Jl. Sultan Muhammad Salahuddin No. 1, Bima, NTB'
+];
